c979fdec394b0ae7aa6a395f95c2d9c8f5ace6d1
elizabethli31 <elizabethli@uchicago.edu>
2022-08-19 13:58:47 -0500

    almost at preemptive

--- crooks.java
+++ crooks.java
@@ -1,28 +1,30 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class crooks {
 
-    static class myPair {
-        private double key;
-        private double value;
+    static class myPair 
+    {
+        int key;
+        int value;
 
-        public myPair(double a, double b)
+        myPair(int a, int b)
         {
             key = a;
             value = b;
         }
 
-        public double key()
+        public int key()
         {
             return key;
         }
-        public double value()
+        public int value()
         {
             return value;
         }
     }
 
-    static class arrayPairs {
+    static class arrayPairs 
+    {
         private ArrayList<myPair> array = new ArrayList<myPair>();
 
         public ArrayList<myPair> array()
@@ -30,34 +32,14 @@ public class crooks {
             return array;
         }
 
-        // public boolean add(myPair pair)
-        // {
-        //     return possibilities.add(pair);
-        // }
-    }
-
-    static class sets {
-        private myPair coords;
-        private arrayPairs possibilities;
-
-        public sets(myPair cell, arrayPairs choices)
+        public boolean add(myPair pair)
         {
-            coords = cell;
-            possibilities = choices;
-        }
-
-        public myPair coords()
-        {
-            return coords;
-        }
-
-        public arrayPairs possibilities()
-        {
-            return possibilities;
+            return array.add(pair);
         }
     }
 
-    static ArrayList<Integer> onBoard(int[][] board, int row, int col) {
+    static ArrayList<Integer> onBoard(int[][] board, int row, int col) 
+    {
         ArrayList<Integer> included = new ArrayList<Integer>();
         
         // Get row
@@ -81,5 +63,57 @@ public class crooks {
             }
         }
         return included;
+    }
+
+    static ArrayList<Integer> get_possibilities(int[][] board, int row, int col)
+    {
+        ArrayList<Integer> included = onBoard(board, row, col);
+        ArrayList<Integer> not_included = new ArrayList<Integer>();
+
+        for (int i = 1; i <= 9; i++) {
+            if (included.contains(i) == true)
+                not_included.add(i);
+        }
+
+        return not_included;
+    }
+
+    static boolean singleton(ArrayList<Integer> not_included)
+    {
+        if (not_included.size() == 1)
+            return true;
+        else
+            return false;
+    }
+
+    static Map<ArrayList<Integer>, arrayPairs> get_preemptive_sets(int[][] board)
+    {
+        Map<ArrayList<Integer>, arrayPairs> all_possibilities = new HashMap<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != 0)
+                    continue;
+
+                ArrayList<Integer> possibilities = get_possibilities(board, i, j);
+
+                if (singleton(possibilities)) {
+                    board[i][j] = possibilities.get(0);
+                }
+                else
+                {
+                    myPair coords = new myPair(i, j);
+                    arrayPairs values = all_possibilities.get(possibilities);
+
+                    if (values != null) {
+                        values.add(coords);  
+                    }
+                    all_possibilities.put(possibilities, values);
+                }
+            }
+        }
+        // filter to find preemptive sets
+
+
     }
 }
